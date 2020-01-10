a97ceb711135b4758f475ca010b5be35895a7532
Project-82 <59730445+Project-82@users.noreply.github.com>
2020-01-10 12:53:46 +0100

    Update Clustering_evaluation_codesource.R

--- Clustering_evaluation_codesource.R
+++ Clustering_evaluation_codesource.R
@@ -22,8 +22,8 @@ dataset2 = read.table(file= "Tomato_salt stress comparison-1000.txt", header=T, 
 dataset3 = read.table(file="MT_salt stress comparison-1000.txt", header=T, dec=".")      #MT_salt stress is name of dataset3
 
 ### Dataset Prepared 
-dataset <- na.omit(dataset1)      # delet the missing values of dataset1
-dataset <- scale  (dataset1)      # standardize variables of dataset1
+plant_dataset <- na.omit(dataset1)      # delet the missing values of dataset1
+plant_dataset <- scale  (dataset1)      # standardize variables of dataset1
 
 ### Clustering algorithms used
 
@@ -33,13 +33,13 @@ dataset <- scale  (dataset1)      # standardize variables of dataset1
 
 # AGNES clustering
 set.seed(123)
-pam.res<-eclust(dataset, "agnes", k = 5) 
+pam.res<-eclust(plant_dataset, "agnes", k = 5) 
 head(agnes.res$cluster, 15) 
 fviz_cluster(agnes.res) # show clusters
 
 # DIANA clustering
 set.seed(123)
-diana.res<-eclust(dataset, "diana", k = 5) 
+diana.res<-eclust(plant_dataset, "diana", k = 5) 
 head(diana.res$cluster, 15) 
 fviz_cluster(diana.res) # show clusters
 
@@ -47,9 +47,9 @@ fviz_cluster(diana.res) # show clusters
 
 # K-Means clustering
 set.seed(123)
-kmeans.res <- eclust(dataset, "kmeans", k = 5)
+kmeans.res <- eclust(plant_dataset, "kmeans", k = 5)
 head(kmeans.res$cluster, 15)               # shows the 15 instances and in which cluster they were assigned
-fviz_cluster(kmeans.res, dataset)          # visualize clusters
+fviz_cluster(kmeans.res)          # visualize clusters
 
 # PAM clustering
 set.seed(123)
@@ -59,7 +59,7 @@ fviz_cluster(pam.res) # show clusters
 
 # clara clustering
 set.seed(123)
-clara.res<-eclust(dataset, "pam", k = 5) 
+clara.res<-eclust(plant_dataset, "pam", k = 5) 
 head(clara.res$cluster, 15) 
 fviz_cluster(clara.res) # show clusters
 
@@ -67,24 +67,24 @@ fviz_cluster(clara.res) # show clusters
 
 # FANNY clustering
 set.seed(123)
-fanny.res<-eclust(dataset, "fanny", k = 5) 
+fanny.res<-eclust(plant_dataset, "fanny", k = 5) 
 head(fanny.res$cluster, 15) 
 fviz_cluster(fanny.res) # show clusters
 
 ## MODEL CLUSTERING ALGORITHMS
 set.seed(123)
-som.res<- som(dataset, xdim=5, ydim=6)  # specify the x-dimension and the y-dimension of the map
+som.res<- som(plant_dataset, xdim=5, ydim=6)  # specify the x-dimension and the y-dimension of the map
 
 #### Choose the best clustering algorithm with clValid ####
 
 # Internal validation
 
 clmethods    <- c("hierarchical","diana","kmeans","pam","clara","som","fanny")
-internal.val <- clValid(dataset, nClust = 2:10,clMethods = clmethods, maxitems=1000,validation = "internal")
+internal.val <- clValid(plant_dataset, nClust = 2:10,clMethods = clmethods, maxitems=1000,validation = "internal")
 summary(internal.val)
 
 # Relative validation
 
 clmethods     <- c("hierarchical","diana","kmeans","pam","clara","som","fanny")
-stability.val <- clValid(dataset, nClust = 2:10,clMethods = clmethods, maxitems=1000,validation = "stability")
+stability.val <- clValid(plant_dataset, nClust = 2:10,clMethods = clmethods, maxitems=1000,validation = "stability")
 summary(stability.val) 
